793293e0887d1440e2ca659389cd95f501ee56f3
John Raynor <john@raynorj.co.uk>
2026-09-17 11:21:34 +0100

    changed paint in random_rain based on claude more efficient suggestion

--- make_sample_patterns.py
+++ make_sample_patterns.py
@@ -30,28 +30,14 @@ def random_rain(time_ms=50):
         scene.clear()
         scene.hold(100)
         for z in range(SIZE-1,-0,-1):
-            scene.paint(select(lambda px, py, pz: px == x and py == y and pz == z), (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)))
+            scene.paint(column(x, y) & plane("z", z), (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)))
+            #scene.paint(select(lambda px, py, pz: px == x and py == y and pz == z), (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)))
+            #above line retained to remind me how to use select() to pick a single LED, but the column() method is much simpler and more efficient.
             scene.hold(random.randint(30, 500) )
-            scene.paint(select(lambda px, py, pz: px == x and py == y and pz == z), (0,0,0))
-
-            # frame = new_frame(time_ms)
-            # frame["colours"][led_index(x, y, z)] = [150, 150, 150]
-
-        #    yield frame
+            scene.paint(column(x, y) & plane("z", z), (0,0,0))
         scene.fade_in(plane("z", 0), (random.randint(50, 255), random.randint(50, 255), random.randint(50, 255)), steps=7)
         scene.fade(plane("z", 0), (0, 0, 0), steps=20,time_ms=10)
-        
-
-    #scene.paint(plane("z", 0), (255,255,255 ))
-
-
-    # scene = Scene(time_ms)
-    # scene.paint(OUTER, RED)
-    # scene.hold(500)                          # one frame shown for 500 ms
-    # scene.fade_in(MIDDLE, BLUE, steps=20)    # 20 frames, MIDDLE black -> blue
-    # scene.fade_out(ALL, steps=30)            # 30 frames, everything -> black
-
-    return scene.frames
+   return scene.frames
 
 
 def diagonal_wave(time_ms=70):
